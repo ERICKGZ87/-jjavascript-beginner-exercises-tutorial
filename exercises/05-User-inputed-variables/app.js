97638de14a174fef633616025ef798813cb49eb0
ERICKGZ87 <erickgz87@gmail.com>
2021-09-17 22:12:33 +0000

    ejercicios js

--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,8 @@
 var age = prompt('What is your age?');
 
 // Your code below:
+
+
+
+console.log(parseFloat(age)+10)
+
--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,36 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+
+//
+//if (parseFloat(total)>=100){
+ // console.log('Are we there yet')
+//}
+//else if (parseFloat(total)>50){
+
+//console.log('Well be there in 5 minutes')
+//}
+//else if (parseFloat(total)<50){
+  //console.log('Im parking, I see you right now')
+  
+//}
+
+function recorrido(total){
+  let result;
+if (parseFloat(total)>=100 ){
+  result = 'Are we there yet';
+}
+
+else {
+  result = 'Well be there in 5 minutes';
+  return result;
+}
+console.log(total)
+
+
+
+
+
+
+  
